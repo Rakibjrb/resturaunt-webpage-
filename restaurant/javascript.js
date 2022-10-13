6e43d9583eef016c50e33133bbca43c80802d15e
Rakibjrb <78774179+Rakibjrb@users.noreply.github.com>
2022-10-13 21:58:48 +0600

    Add files via upload
    restaurant webpage design using only html css and javascript full source code are here

---
+++ restaurant/javascript.js
@@ -0,0 +1,11 @@
+let menuIcon = document.getElementById('open');
+let menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', () => {
+    if(menu.className == 'hidden'){
+        menu.classList.remove('hidden');
+    }
+    else{
+        menu.classList.add('hidden');
+    }
+});
